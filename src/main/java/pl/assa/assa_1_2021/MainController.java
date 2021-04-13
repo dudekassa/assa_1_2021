286b16cbeaac750f189866148fb2bc42763f080c
Przemysław Dudek <dudek.mail@gmail.com>
2021-04-13 09:09:59 +0200

    FEAT: MVC Lekcja 3 - Zadanie 01 - 13-04-2021

--- src/main/java/pl/assa/assa_1_2021/MainController.java
+++ src/main/java/pl/assa/assa_1_2021/MainController.java
@@ -10,4 +10,9 @@ public class MainController {
     public String index() {
         return "index";
     }
+
+    @GetMapping("/zadanie_1")
+    public String zadanie01() {
+        return "zadanie_1";
+    }
 }
