7ccc99669a2e97af5f8486c1921fa195284f0cd2
Przemysław Dudek <dudek.mail@gmail.com>
2021-05-11 09:12:31 +0200

    FEAT: MVC Lekcja 4 - Init Zadanie 02 - 11-05-2021

--- src/main/java/pl/assa/assa_1_2021/MainController.java
+++ src/main/java/pl/assa/assa_1_2021/MainController.java
@@ -1,13 +1,22 @@
 package pl.assa.assa_1_2021;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class MainController {
 
     @GetMapping("/")
     public String index() {
+        return "index";
+    }
+
+    @PostMapping("/zadanie_02")
+    public String zadanie02(@RequestParam(name = "name") String name, Model model) {
+        model.addAttribute("nameLength", name.length());
         return "index";
     }
 }
